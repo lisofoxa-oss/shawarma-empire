454d75a356ec10c697a4ca02e54f2db4fe9cbdef
lisofoxa-oss <lisofoxa@gmail.com>
2026-01-23 18:31:59 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Запуск игры
+
+// Ждём полной загрузки DOM
+document.addEventListener('DOMContentLoaded', () => {
+  // Инициализируем игру
+  Game.init();
+  
+  console.log('🌯 Империя Шаурмы запущена!');
+  console.log('Версия: 1.0.0');
+  console.log('GitHub: https://github.com/your-username/shawarma-empire');
+});
